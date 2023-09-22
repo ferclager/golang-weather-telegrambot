486e66027c932024efac44b9033a701f1f8a587b
FeR Clager <ferclager@users.noreply.github.com>
2023-09-22 13:46:26 +0200

    Add package modules

---
+++ models/models.go
@@ -0,0 +1,98 @@
+package models
+
+type ForecastAPIResponse struct {
+	Cod     string `json:"cod"`
+	Message int    `json:"message"`
+	Cnt     int    `json:"cnt"`
+	List    []struct {
+		Dt   int `json:"dt"`
+		Main struct {
+			Temp      float64 `json:"temp"`
+			FeelsLike float64 `json:"feels_like"`
+			TempMin   float64 `json:"temp_min"`
+			TempMax   float64 `json:"temp_max"`
+			Pressure  int     `json:"pressure"`
+			SeaLevel  int     `json:"sea_level"`
+			GrndLevel int     `json:"grnd_level"`
+			Humidity  int     `json:"humidity"`
+			TempKf    float64 `json:"temp_kf"`
+		} `json:"main"`
+		Weather []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Clouds struct {
+			All int `json:"all"`
+		} `json:"clouds"`
+		Wind struct {
+			Speed float64 `json:"speed"`
+			Deg   int     `json:"deg"`
+		} `json:"wind"`
+		Visibility int     `json:"visibility"`
+		Pop        float64 `json:"pop"`
+		Sys        struct {
+			Pod string `json:"pod"`
+		} `json:"sys"`
+		DtTxt string `json:"dt_txt"`
+		Rain  struct {
+			ThreeH float64 `json:"3h"`
+		} `json:"rain,omitempty"`
+	} `json:"list"`
+	City struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Coord struct {
+			Lat float64 `json:"lat"`
+			Lon float64 `json:"lon"`
+		} `json:"coord"`
+		Country    string `json:"country"`
+		Population int    `json:"population"`
+		Timezone   int    `json:"timezone"`
+		Sunrise    int    `json:"sunrise"`
+		Sunset     int    `json:"sunset"`
+	} `json:"city"`
+}
+
+type WeatherAPIResponse struct {
+	Base   string `json:"base"`
+	Clouds struct {
+		All int64 `json:"all"`
+	} `json:"clouds"`
+	Cod   int64 `json:"cod"`
+	Coord struct {
+		Lat float64 `json:"lat"`
+		Lon float64 `json:"lon"`
+	} `json:"coord"`
+	Dt   int64 `json:"dt"`
+	ID   int64 `json:"id"`
+	Main struct {
+		FeelsLike float64 `json:"feels_like"`
+		Humidity  int64   `json:"humidity"`
+		Pressure  int64   `json:"pressure"`
+		Temp      float64 `json:"temp"`
+		TempMax   float64 `json:"temp_max"`
+		TempMin   float64 `json:"temp_min"`
+	} `json:"main"`
+	Name string `json:"name"`
+	Sys  struct {
+		Country string `json:"country"`
+		ID      int64  `json:"id"`
+		Sunrise int64  `json:"sunrise"`
+		Sunset  int64  `json:"sunset"`
+		Type    int64  `json:"type"`
+	} `json:"sys"`
+	Timezone   int64 `json:"timezone"`
+	Visibility int64 `json:"visibility"`
+	Weather    []struct {
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+		ID          int64  `json:"id"`
+		Main        string `json:"main"`
+	} `json:"weather"`
+	Wind struct {
+		Deg   int64   `json:"deg"`
+		Speed float64 `json:"speed"`
+	} `json:"wind"`
+}
